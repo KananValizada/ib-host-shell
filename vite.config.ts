004bffdfd4c1d37f3ae7e6d214c740f85262ff9a
kananvalizada <004163@ibar.int>
2026-04-16 11:10:51 +0400

    configure remote URLs

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'host_shell',
-      remotes: {
-      remote_accounts: 'http://localhost:3001/assets/remoteEntry.js',
-      remote_cards: 'http://localhost:3002/assets/remoteEntry.js'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'host_shell',
+        remotes: {
+          remote_accounts: env.VITE_REMOTE_ACCOUNTS,
+          remote_cards: env.VITE_REMOTE_CARDS,
+        },
+        shared: ['react', 'react-dom'],
+      }),
+    ],
+    build: {
+      target: 'esnext',
     },
-      shared: ['react', 'react-dom']
-    })
-  ],
-  build: {
-    target: 'esnext'
-  },
-  server: {
-    port: 3000
-  }
+    server: {
+      port: 3000,
+    },
+  };
 });
