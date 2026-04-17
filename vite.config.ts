c5bcdc4a8d7d9981570826f76fb9a8deb143752d
kananvalizada <004163@ibar.int>
2026-04-17 10:23:59 +0400

    chore: align shared dependencies with remotes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,17 @@ export default defineConfig(({ mode }) => {
           remote_accounts: env.VITE_REMOTE_ACCOUNTS,
           remote_cards: env.VITE_REMOTE_CARDS,
         },
-        shared: ['react', 'react-dom'],
+         shared: {
+          react: {
+            version: '^19.2.4',
+          },
+          'react-dom': {
+            version: '^19.2.4',
+          },
+          'react-router-dom': {
+            version: '^7.14.1',
+          },
+        },
       }),
     ],
     build: {
